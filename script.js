0987bbfb993d6a43dc96a69e517087b9247e97b8
jcr7306 <peppy7306@gmail.com>
2026-07-13 19:40:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function showMessage(){
+function showMessage() {
 
-alert("Let's start building your gaming PC!");
+    let parts = [
+        "CPU",
+        "GPU",
+        "Motherboard",
+        "RAM",
+        "Storage",
+        "Power Supply"
+    ];
+
+    let message = "Let's start building your gaming PC!\n\n";
+    message += "These are the main parts you'll need:\n\n";
+
+    for(let i = 0; i < parts.length; i++){
+        message += "- " + parts[i] + "\n";
+    }
+
+    alert(message);
 
 }
